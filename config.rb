bc3c14f1b233d17f1fd9c8f9942fe1e7ecc0fb9f
kei <kei.shiratsuchi@gmail.com>
2013-07-26 14:31:48 +0900

    Publish to mrtaddy/omg-tech

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/omg-tech/"
+http_images_path = "/omg-tech/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/omg-tech/fonts"
+css_dir = "public/omg-tech/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
